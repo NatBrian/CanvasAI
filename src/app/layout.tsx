e11c20a5f055042485c5c093fd69474836d0ba4d
N B <Brian.Atmawidjaja@gmail.com>
2025-07-01 17:56:58 +0000

    add vercel analytics to my project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'CanvasAI',
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className="font-body antialiased overflow-hidden">
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
